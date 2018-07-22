6cbdc9b307e6b5462009cdba3c37d2499d0aa325
cyrusn <cyrusncy@gmail.com>
2018-07-23 00:16:17 +0800

    minor bug fix

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import sys
 
 query = sys.argv[1]
 
-
 dollars = re.split(r"\n|\,\s|\;|\s|\+", query)
 
 result = 0.0
@@ -17,6 +16,5 @@ for dollar in dollars:
     var = re.search(reg, dollar)
     if var != None:
         result += locale.atof(var['number'])
-
 
 print(result, end='')
